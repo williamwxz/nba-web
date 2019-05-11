6fdc08b203866a403be6a3db80eab95073188382
William Zhang <zhawill@amazon.com>
2019-05-10 17:03:59 -0700

    change player name as constant

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,18 @@ import { DataViewContainer } from './DataViewContainer';
 import nba from 'nba'
 import { Profile } from './Profile';
 
+const playerName = 'lebron james'
 
 export class Main extends React.Component {
     state = {
         playerInfo: {
-            playerId: nba.findPlayer('Stephen Curry').playerId,
+            playerId: nba.findPlayer(playerName).playerId,
             teamAbbreviation: 'GSW',
         }
     }
 
     componentDidMount() {
-        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId }).then((info) => {
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId }).then((info) => {
             console.log(info);
             const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
             console.log(playerInfo);
